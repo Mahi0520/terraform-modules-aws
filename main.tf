242f1280cdfe7a9932c1c963155428d4fe6d83c4
Mahi0520 <130120995+Mahi0520@users.noreply.github.com>
2024-02-29 13:08:04 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,22 @@ module "s3_bucket" {
 
 module "iam_user1" {
   source = "./modules/iam"
-  user_name = "usermahi88"
+  user_name = "usermahi123"
   group_membership = ["group1", "group2"]
+}
+
+module "elb" {
+  source = "./modules/load_balancer"
+  elb_name = "mahilb"
+  elb_subnets = ["subnet-0c40ec2bd4a6078d9","subnet-06ee3c6c63f90a17f"]
+  elb_security_groups = ["sg-06a13f867df186b2d","sg-03cd61c753e518e4d"]
+}
+
+module "targetgroup" {
+  source = "./modules/targetgroup"
+  target_group_name = "mahitg"
+  target_group_type = "instance"
+  vpc_id = "vpc-061227211d98d1184"
+  target_group_port = "80"
+
 }
