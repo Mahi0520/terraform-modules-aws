b6ed37740522d8bbae13f73189f6ecab10b25aa4
Mahi0520 <130120995+Mahi0520@users.noreply.github.com>
2024-02-29 11:23:48 -0600

    variables & ouput in ec2

---
+++ modules/ec2_instance/outputs.tf
@@ -0,0 +1,3 @@
+output "public_ip-address" {
+  value = aws_instance.demo.public_ip
+}
---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+  description = "value of ami"
+}
+variable "instance_type_value" {
+  description = "value of type"
+}
+variable "subnet_id_value" {
+  description = "value for subnet"
+}
