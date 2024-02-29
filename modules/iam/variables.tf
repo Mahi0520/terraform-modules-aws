81650d8b02451bcd7c47c01314f34be303575bfc
Mahi0520 <130120995+Mahi0520@users.noreply.github.com>
2024-02-29 12:42:53 -0600

    Update variables.tf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,10 +1,10 @@
-# variable "user_name" {
-#   description = "The name of the IAM user"
-# }
+variable "user_name" {
+  description = "The name of the IAM user"
+}
 
-# variable "group_membership" {
-#   description = "List of IAM groups the user should be a member of"
-#   type        = list(string)
-# }
+variable "group_membership" {
+  description = "List of IAM groups the user should be a member of"
+  type        = list(string)
+}
 
-# # Add more variables as needed
+# Add more variables as needed
