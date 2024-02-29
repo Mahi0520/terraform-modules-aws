e5228d42fad86606481b37d921bc1ab7418280af
Mahi0520 <130120995+Mahi0520@users.noreply.github.com>
2024-02-29 13:06:50 -0600

    tg-variables

---
+++ modules/target_group/variables.tf
@@ -0,0 +1,13 @@
+variable "target_group_name" {
+  description = "Name of the tg"
+  type        = string
+}
+variable "target_group_type" {
+  
+}
+variable "target_group_port" {
+  description = "port number "
+}
+variable "vpc_id" {
+  
+}
